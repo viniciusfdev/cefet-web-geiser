fa8a813aa34c536b00c3a2bc9ecab668a6dec3bd
Vinicius França <viniciusfdev@gmail.com>
2021-07-31 14:36:26 -0300

    exercicio 1

--- server/app.js
+++ server/app.js
@@ -1,23 +1,21 @@
 // importação de dependência(s)
-
+import express from "express";
+import path from "path";
 
 // variáveis globais deste módulo
-const PORT = 3000
-const db = {}
-
+const PORT = 3000;
+const db = {};
+const app = express();
+const __dirname = path.resolve();
 
 // carregar "banco de dados" (data/jogadores.json e data/jogosPorJogador.json)
 // você pode colocar o conteúdo dos arquivos json no objeto "db" logo abaixo
 // dica: 1-4 linhas de código (você deve usar o módulo de filesystem (fs))
 
-
-
-
 // configurar qual templating engine usar. Sugestão: hbs (handlebars)
 //app.set('view engine', '???qual-templating-engine???');
 //app.set('views', '???caminho-ate-pasta???');
 // dica: 2 linhas
-
 
 // EXERCÍCIO 2
 // definir rota para página inicial --> renderizar a view index, usando os
@@ -25,19 +23,20 @@ const db = {}
 // dica: o handler desta função é bem simples - basta passar para o template
 //       os dados do arquivo data/jogadores.json (~3 linhas)
 
-
-
 // EXERCÍCIO 3
 // definir rota para página de detalhes de um jogador --> renderizar a view
 // jogador, usando os dados do banco de dados "data/jogadores.json" e
 // "data/jogosPorJogador.json", assim como alguns campos calculados
 // dica: o handler desta função pode chegar a ter ~15 linhas de código
 
-
 // EXERCÍCIO 1
 // configurar para servir os arquivos estáticos da pasta "client"
 // dica: 1 linha de código
 
+app.use(express.static(`${__dirname}/client`));
 
 // abrir servidor na porta 3000 (constante PORT)
 // dica: 1-3 linhas de código
+const server = app.listen(PORT, () =>
+  console.log(`Server start listing on port ${PORT}`)
+);
