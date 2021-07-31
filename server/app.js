d935b390784955f89937a1d9c61c4d80ec015b31
Vinicius França <viniciusfdev@gmail.com>
2021-07-31 18:26:34 -0300

    exercicio 2

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 // importação de dependência(s)
 import express from "express";
 import path from "path";
+import { loadData } from "./utils.js";
 
 // variáveis globais deste módulo
 const PORT = 3000;
@@ -8,20 +9,19 @@ const db = {};
 const app = express();
 const __dirname = path.resolve();
 
-// carregar "banco de dados" (data/jogadores.json e data/jogosPorJogador.json)
-// você pode colocar o conteúdo dos arquivos json no objeto "db" logo abaixo
-// dica: 1-4 linhas de código (você deve usar o módulo de filesystem (fs))
-
-// configurar qual templating engine usar. Sugestão: hbs (handlebars)
-//app.set('view engine', '???qual-templating-engine???');
-//app.set('views', '???caminho-ate-pasta???');
-// dica: 2 linhas
-
 // EXERCÍCIO 2
 // definir rota para página inicial --> renderizar a view index, usando os
 // dados do banco de dados "data/jogadores.json" com a lista de jogadores
 // dica: o handler desta função é bem simples - basta passar para o template
 //       os dados do arquivo data/jogadores.json (~3 linhas)
+loadData("jogadores").then((data) => (db["jogadores"] = data));
+loadData("jogosPorJogador").then((data) => (db["jogosPorJogador"] = data));
+app.set("view engine", "hbs");
+app.set("views", "views");
+
+app.get("/", (request, response) => {
+  response.render("index", db);
+});
 
 // EXERCÍCIO 3
 // definir rota para página de detalhes de um jogador --> renderizar a view
@@ -32,11 +32,8 @@ const __dirname = path.resolve();
 // EXERCÍCIO 1
 // configurar para servir os arquivos estáticos da pasta "client"
 // dica: 1 linha de código
-
 app.use(express.static(`${__dirname}/client`));
 
 // abrir servidor na porta 3000 (constante PORT)
 // dica: 1-3 linhas de código
-const server = app.listen(PORT, () =>
-  console.log(`Server start listing on port ${PORT}`)
-);
+app.listen(PORT, () => console.log(`Server start listing on port ${PORT}`));
---
+++ server/utils.js
@@ -0,0 +1,10 @@
+import { readFile } from "fs";
+
+export async function loadData(name) {
+  return new Promise((resolve, reject) => {
+    readFile(`data/${name}.json`, (err, data) => {
+      if (err) reject(err);
+      else resolve(JSON.parse(data.toString("utf8")));
+    });
+  });
+}
