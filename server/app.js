9ebc483d45d9aa8008ef7b717c80d67c1a76fbd9
Vinicius França <viniciusfdev@gmail.com>
2021-07-31 19:23:35 -0300

    exercicio 3

--- server/app.js
+++ server/app.js
@@ -17,7 +17,7 @@ const __dirname = path.resolve();
 loadData("jogadores").then((data) => (db["jogadores"] = data));
 loadData("jogosPorJogador").then((data) => (db["jogosPorJogador"] = data));
 app.set("view engine", "hbs");
-app.set("views", "views");
+app.set("views", "server/views");
 
 app.get("/", (request, response) => {
   response.render("index", db);
@@ -28,6 +28,25 @@ app.get("/", (request, response) => {
 // jogador, usando os dados do banco de dados "data/jogadores.json" e
 // "data/jogosPorJogador.json", assim como alguns campos calculados
 // dica: o handler desta função pode chegar a ter ~15 linhas de código
+app.get("/jogador/:id", (request, response) => {
+  const id = request.params.id;
+  const player = db["jogadores"]?.players.find((j) => j.steamid === id);
+  if (!player) return response.status(404).end();
+
+  const metrics = {};
+  const rawMetrics = db["jogosPorJogador"]?.[id];
+
+  metrics.mostPlayed = rawMetrics.games
+    .sort((a, b) => b.playtime_forever - a.playtime_forever)
+    .slice(0, 5)
+    .map((game) => ({ ...game, playtime: `${Math.floor(game.playtime_forever / 60)}H` }));
+
+  metrics.favoriteGame = metrics.mostPlayed[0];
+  metrics.gameCount = rawMetrics.game_count;
+  metrics.notPlayed = rawMetrics.games.filter((g) => !!g.playtime_forever).length;
+
+  response.render("jogador", { player, metrics });
+});
 
 // EXERCÍCIO 1
 // configurar para servir os arquivos estáticos da pasta "client"
--- server/utils.js
+++ server/utils.js
@@ -2,7 +2,7 @@ import { readFile } from "fs";
 
 export async function loadData(name) {
   return new Promise((resolve, reject) => {
-    readFile(`data/${name}.json`, (err, data) => {
+    readFile(`server/data/${name}.json`, (err, data) => {
       if (err) reject(err);
       else resolve(JSON.parse(data.toString("utf8")));
     });
